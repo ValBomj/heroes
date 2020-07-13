86bb5dc64bc406e7ca8c820c30ac448b6c55e6d6
ValBomj <123itunes2002@gmail.com>
2020-07-13 05:53:40 +0300

    created 2 version of heroes project

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const select = document.getElementById("select"),
   body = document.querySelector("body"),
   heroesInner = document.querySelector(".heroes__inner");
 
-//////////////////////////////////////////////////////////////////////////////////
 
 const getData = () => {
   start.style.display = "none";
@@ -22,98 +21,137 @@ const getData = () => {
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
       const data = JSON.parse(request.responseText);
-
-      addCard(data);
-      let filteredMovies = [];
-
-      data.forEach((item) => {
-        select.innerHTML = `<option value="0">Chose film</option>`;
-        if (item.movies) {
-          item.movies.forEach((item) => {
-            filteredMovies.push(item);
-          });
-        }
-        filteredMovies = new Set(filteredMovies);
-        filteredMovies = [...filteredMovies];
-        filteredMovies.forEach((item) => {
-          const movie = document.createElement("option");
-          movie.textContent = item;
-          movie.value = item;
-          select.appendChild(movie);
-        });
-      });
-
-      select.addEventListener("change", () => {
-        addCard(data, select.value);
-      });
+      getFilms(data);
+      createCard(data);
     }
   });
 };
 
-const addCard = (data, selectValue) => {
-  heroesInner.innerHTML = "";
+const getFilms = (data) => {
+  const movies = [];
+
   data.forEach((item) => {
+    select.innerHTML = `<option value="0">Chose film</option>`;
     if (item.movies) {
-      item.movies.forEach((movieItem) => {
-        if (movieItem === selectValue) {
-          const card = document.createElement("div");
-          card.className = "heroes__card";
-
-          const { photo, name, movies, status, actors, birthDay, deathDay, ...options } = item;
-          let moviesList = [];
-
-          if (movies) {
-            movies.forEach((item) => {
-              if (item === select.value) {
-              }
-              item = `<li>${item}</li>`;
-              moviesList.push(item);
-            });
-            moviesList = moviesList.join("");
-          } else {
-            moviesList = `There are no films yet`;
-          }
-
-          let lifePeriod = "Unknown";
-
-          if (birthDay && deathDay) {
-            lifePeriod = `${birthDay} - ${deathDay}`;
-          }
-
-          let op = [];
-          for (const key in options) {
-            item = `<li>${key} - ${options[key]}</li>`;
-            op.push(item);
-          }
-          op = op.join("");
-
-          card.innerHTML = `
-            <div class="heroes__card-image" style="background-image: url(${photo});"></div>
-            <div class="heroes__card-inner">
-              <div class="heroes__card-descr">
-                <div class="heroes__card-name title">${name}</div>
-                <div class="heroes__card-actor">Actor: ${actors}</div>
-                <div class="heroes__card-period">Life Period: ${lifePeriod}</div>
-              </div>
-              <div class="heroes__card-info-title title">Information</div>
-              <div class="heroes__card-info">
-                <ul>
-                  ${op}
-                </ul>
-              </div>
-              <div class="heroes__card-movies-title title">Film List</div>
-              <div class="heroes__card-movies">
-                <ul>
-                  ${moviesList}
-                </ul>
-              </div>
-            </div>
-        `;
-          heroesInner.appendChild(card);
-        }
+      item.movies.forEach((item) => {
+        movies.push(item);
       });
     }
+    const filteredMovies = new Set(movies);
+    [...filteredMovies].forEach((item) => {
+      const movie = document.createElement("option");
+      movie.textContent = item;
+      movie.value = item;
+      select.appendChild(movie);
+    });
   });
-};
+
+  select.addEventListener("change", () => {
+      createCard(data, select.value);
+  });
+}
+
+class Card{
+  constructor({photo = './card-bg/default.png', name, movies = [], actors, birthDay, deathDay = 'Still alive', ...options}) {
+    this.photo = photo;
+    this.name = name;
+    this.movies = movies;
+    this.actors = actors;
+    this.birthDay = birthDay;
+    this.deathDay = deathDay;
+    this.lifePeriod = '';
+    this.moviesList = [];
+    this.options = options;
+    this.optionsList = [];
+    this.card = document.createElement("div");
+  }
+  init() {
+    this.getCardMovies();
+    this.getLifePeriod();
+    this.getOptions();
+  }
+
+  getCardMovies() {
+    if (this.movies.length > 0) {
+      this.movies.forEach((item) => {
+        item = `<li>${item}</li>`;
+        this.moviesList.push(item);
+      });
+      this.moviesList = this.moviesList.join('');
+    } else {
+      this.moviesList = 'There are no films yet';
+    }
+  }
+
+  getLifePeriod() {
+    this.lifePeriod = this.birthDay ? `${this.birthDay} - ${this.deathDay}` : 'Unknown';
+  }
+
+  getOptions() {
+    for (const key in this.options) {
+      const option = `<li>${key} - ${this.options[key]}</li>`;
+      this.optionsList.push(option);
+    }
+    this.optionsList = this.optionsList.join("");
+  }
+
+  createCard(selectValue) {
+    this.card.className = "heroes__card";
+    this.card.innerHTML = `
+      <div class="heroes__card-image" style="background-image: url(${this.photo});"></div>
+      <div class="heroes__card-inner">
+        <div class="heroes__card-descr">
+          <div class="heroes__card-name title">${this.name}</div>
+          <div class="heroes__card-actor">Actor: ${this.actors}</div>
+          <div class="heroes__card-period">Life Period: ${this.lifePeriod}</div>
+        </div>
+        <div class="heroes__card-info-title title">Information</div>
+        <div class="heroes__card-info">
+          <ul>
+            ${this.optionsList}
+          </ul>
+        </div>
+        <div class="heroes__card-movies-title title">Film List</div>
+        <div class="heroes__card-movies">
+          <ul>
+            ${this.moviesList}
+          </ul>
+        </div>
+      </div>
+    `;
+    heroesInner.appendChild(this.card);
+  }
+}
+
+const createCard = (data, selectValue) => {
+  heroesInner.innerHTML = '';
+  data.forEach(item => {
+    const newCard = new Card(item);
+    newCard.init();
+
+    if (selectValue && selectValue !== '0') {
+      if (newCard.movies) {
+        newCard.movies.forEach(movie => {
+          if (movie === selectValue) {
+            newCard.createCard(selectValue);
+          }
+        })
+      }
+    } else {
+      newCard.createCard(selectValue);
+    }
+  })
+}
+
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  if (target.closest('.heroes__card')) {
+    target = target.closest('.heroes__card');
+    target.style.zIndex = target.style.zIndex === '10' ? 1 : 10;
+    console.log( target.style.zIndex);
+    target.classList.contains('floating') ? target.classList.remove('floating') : target.classList.add('floating');
+  } 
+})
 
 start.addEventListener("click", getData);
