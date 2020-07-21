bc4e32b12484990c00a8771556da0a5997d77a09
ValBomj <123itunes2002@gmail.com>
2020-07-21 09:47:12 +0300

    version1.0.2

--- js/app.js
+++ js/app.js
@@ -2,54 +2,89 @@
 
 const select = document.getElementById("select"),
   start = document.getElementById("start"),
+  error = document.getElementById("error"),
   body = document.querySelector("body"),
-  heroesInner = document.querySelector(".heroes__inner");
+  heroesInner = document.querySelector(".heroes__inner"),
+  url = './dbHeroes.json';
 
 
-const getData = () => {
-  start.style.display = "none";
-  select.style.display = "block";
+// const getData = () => {
+//   return fetch(url);
+// }
 
-  const request = new XMLHttpRequest();
+// const toHTML = (hero, selectValue) => {
+//   const {photo = './card-bg/default.png', name, movies = [], actors, birthDay, deathDay = 'Still alive', ...options} = hero,
+//     lifePeriod = birthDay ? `${birthDay} - ${deathDay}` : 'Unknown',
+//     moviesList = movies.length > 0 ? movies.map(movie => `<li>${movie}</li>`).join('') : 'There are no films yet',
+//     optionsList = [];
 
-  request.open("GET", "dbHeroes.json");
+//   for (const key in options) {
+//     const option = `<li>${key} - ${options[key]}</li>`;
+//     optionsList.push(option);
+//   }
+//   // optionsList = optionsList.join("");
 
-  request.setRequestHeader("Content-Type", "application/json");
+//   return `
+//     <div class="heroes__card">
+//       <div class="heroes__card-image" style="background-image: url(${photo});"></div>
+//       <div class="heroes__card-inner">
+//         <div class="heroes__card-descr">
+//           <div class="heroes__card-name title">${name}</div>
+//           <div class="heroes__card-actor">Actor: ${actors}</div>
+//           <div class="heroes__card-period">Life Period: ${lifePeriod}</div>
+//         </div>
+//         <div class="heroes__card-info-title title">Information</div>
+//         <div class="heroes__card-info">
+//           <ul>
+//             ${optionsList.join("")}
+//           </ul>
+//         </div>
+//         <div class="heroes__card-movies-title title">Film List</div>
+//         <div class="heroes__card-movies">
+//           <ul>
+//             ${moviesList}
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   `
+// }
 
-  request.send();
+// const render = response => {
+//   const html = response.map(toHTML);
+//   let moviesList = [];
+//   response.forEach(item => {
+//     if (item.movies) {
+//       moviesList.push(item.movies)
+//     }
+//   })
+//   console.log(moviesList);
+//   // const movies = response.map(hero => hero.movies ? hero.movies.join(', ') : '');
+//   // const filteredMovies = new Set(movies);
+//   console.log(filteredMovies);
+//   document.querySelector(".heroes__inner").innerHTML = html;
+// }
 
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      getFilms(data);
-      createCard(data);
-    }
-  });
-};
+// const getMovies = response => {
+//   if (response)
+//   if (!response.movies) return;
+//   return response.movies;
+// }
 
-const getFilms = (data) => {
-  const movies = [];
 
-  data.forEach((item) => {
-    select.innerHTML = `<option value="0">Chose film</option>`;
-    if (item.movies) {
-      item.movies.forEach((item) => {
-        movies.push(item);
-      });
-    }
-    const filteredMovies = new Set(movies);
-    [...filteredMovies].forEach((item) => {
-      const movie = document.createElement("option");
-      movie.textContent = item;
-      movie.value = item;
-      select.appendChild(movie);
-    });
-  });
 
-  select.addEventListener("change", () => {
-      createCard(data, select.value);
-  });
-}
+// start.addEventListener('click', () => {
+//   start.style.display = 'none';
+//   select.style.display = 'block';
+//   getData()
+//     .then(response =>  response.json())
+//     .then(render)
+//     .catch(error => console.error(`There is an error: ${error}`))
+// })
+
+
+const getData = () => fetch(url);
+
 
 class Card{
   constructor({photo = './card-bg/default.png', name, movies = [], actors, birthDay, deathDay = 'Still alive', ...options}) {
@@ -57,34 +92,14 @@ class Card{
     this.name = name;
     this.movies = movies;
     this.actors = actors;
-    this.birthDay = birthDay;
-    this.deathDay = deathDay;
-    this.lifePeriod = '';
-    this.moviesList = [];
+    this.moviesList = movies.length > 0 ? movies.map(movie => `<li>${movie}</li>`).join('') : 'There are no films yet';
     this.options = options;
     this.optionsList = [];
+    this.lifePeriod = birthDay ? `${birthDay} - ${deathDay}` : 'Unknown';
     this.card = document.createElement("div");
   }
   init() {
-    this.getCardMovies();
-    this.getLifePeriod();
     this.getOptions();
-  }
-
-  getCardMovies() {
-    if (this.movies.length > 0) {
-      this.movies.forEach((item) => {
-        item = `<li>${item}</li>`;
-        this.moviesList.push(item);
-      });
-      this.moviesList = this.moviesList.join('');
-    } else {
-      this.moviesList = 'There are no films yet';
-    }
-  }
-
-  getLifePeriod() {
-    this.lifePeriod = this.birthDay ? `${this.birthDay} - ${this.deathDay}` : 'Unknown';
   }
 
   getOptions() {
@@ -123,9 +138,34 @@ class Card{
   }
 }
 
-const createCard = (data, selectValue) => {
+
+const getFilms = response => {
+  const movies = [];
+
+  response.forEach((item) => {
+    select.innerHTML = `<option value="0">Chose film</option>`;
+    if (item.movies) {
+      item.movies.forEach((item) => {
+        movies.push(item);
+      });
+    }
+    const filteredMovies = new Set(movies);
+    [...filteredMovies].forEach((item) => {
+      const movie = document.createElement("option");
+      movie.textContent = item;
+      movie.value = item;
+      select.appendChild(movie);
+    });
+  });
+
+  select.addEventListener("change", () => {
+      createCard(response, select.value);
+  });
+}
+
+const createCard = (response, selectValue) => {
   heroesInner.innerHTML = '';
-  data.forEach(item => {
+  response.forEach(item => {
     const newCard = new Card(item);
     newCard.init();
 
@@ -133,25 +173,29 @@ const createCard = (data, selectValue) => {
       if (newCard.movies) {
         newCard.movies.forEach(movie => {
           if (movie === selectValue) {
-            newCard.createCard(selectValue);
+            newCard.createCard();
           }
         })
       }
     } else {
-      newCard.createCard(selectValue);
+      newCard.createCard();
     }
   })
 }
 
+start.addEventListener("click", () => {
 
-document.addEventListener('click', e => {
-  let target = e.target;
-  if (target.closest('.heroes__card')) {
-    target = target.closest('.heroes__card');
-    target.style.zIndex = target.style.zIndex === '10' ? 1 : 10;
-    console.log( target.style.zIndex);
-    target.classList.contains('floating') ? target.classList.remove('floating') : target.classList.add('floating');
-  } 
-})
-
-start.addEventListener("click", getData);
+  getData()
+    .then(response => {
+      start.style.display = "none";
+      select.style.display = "block";
+      return response.json();
+    })
+    .then(response => {
+      getFilms(response);
+      createCard(response);
+    })
+    .catch(err => {
+      heroesInner.textContent = `Что-то пошло не так... Ошибка: ${err}`;
+    })
+});
